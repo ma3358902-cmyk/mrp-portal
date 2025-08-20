2c212e09c4c40d09f33adfc3ad6e8a2ba8d6fb17
ma3358902-cmyk <ma3358902@gmail.com>
2025-08-21 03:36:21 +0800

    Update app.module.ts

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -3,11 +3,11 @@ import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { MasterModule } from './master/master.module';
 import { PlanModule } from './plan/plan.module';
-import { RMModule } from './rm/rm.module';
+import { RmModule } from './rm/rm.module';
 import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [AuthModule, MasterModule, PlanModule, RMModule, ReportModule],
+  imports: [AuthModule, MasterModule, PlanModule, RmModule, ReportModule],
   providers: [PrismaService],
 })
 export class AppModule {}
